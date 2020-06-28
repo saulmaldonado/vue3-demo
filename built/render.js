2081ea55bd1990af5a60c6051251579255cafa14
Saul Maldonado <samaldonado4@gmail.com>
2020-06-28 05:36:54 -0500

    Ignore js build folder

--- built/render.js
+++
@@ -1,8 +0,0 @@
-var h = function (tag, props, children) {
-    return {
-        tag: tag,
-        props: props,
-        children: children,
-    };
-};
-export { h };
--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,16 @@ import { patch } from './patch';
 // My example at recreating render and mounting functions
 
 const vdom1 = h('div', { class: 'red' }, [h('span', null, 'hello')]);
-/**
- * result
+/** initial node
  * <div id="app">
  *   <div class="red">hello</div>
  * </div>
  */
 mount(vdom1, document.getElementById('app'));
 
-//create function that will compare and the two tree and make the changes needed
-
 /* Cases */
+
+//-------------------------------------------------------------------------------------------------------
 
 const vdom2 = h('div', { class: 'red' }, [h('span', null, 'changed!')]);
 
